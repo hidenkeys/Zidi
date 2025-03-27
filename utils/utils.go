282172c31583ebbde6c4b307edf0cfa8895b4f71
hidenkeys <letimapro23@gmail.com>
2025-03-27 11:10:47 +0100

    customers

--- utils/utils.go
+++ utils/utils.go
@@ -220,11 +220,9 @@ func CreateFlutterwavePaymentLink(email string, amount int, campaignID, organiza
 		"currency":     "NGN",
 		"redirect_url": "https://yourwebsite.com/payment-success",
 		"customer": map[string]string{
-			"email":           email,
-			"campaign_id":     campaignID,
-			"organization_id": organizationID,
+			"email": email,
 		},
-		"meta_data": map[string]string{
+		"meta": map[string]string{
 			"campaign_id":     campaignID,
 			"organization_id": organizationID,
 		},
